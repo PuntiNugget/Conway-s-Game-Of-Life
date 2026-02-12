1df97123fd2fdd2ff84119586b032032a5e6f967
PuntiNugget <pbura@students.wcpss.net>
2026-02-12 08:20:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let count = 0;
 
-const ClickButton = document.getElementById('Ollie')
-const clickDisplay = document.getElementByID('
+const ClickButton = document.getElementById('Ollie');
+const clickDisplay = document.getElementByID('output');
+
+ClickButton.addEventListener('Click', function(){
+  count++
+
+  clickDisplay.textcontent = count;
+});
